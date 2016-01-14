74aa8c89b4ff5d813c09adf78e4eafe4cccabc4e
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-15 00:30:30 +0530

    Uploaded Answer 3

---
+++ A1-Q1A3.C
@@ -0,0 +1,14 @@
+# include<stdio.h>
+# include<conio.h>
+void main()
+{
+int mul,a,b;
+clrscr();
+printf("enter the no.");
+scanf("%d",&a);
+printf("enter 2nd no.");
+printf("%d",b);
+mul=a*b;
+printf("value is \t %d",mul);
+getch();
+}
