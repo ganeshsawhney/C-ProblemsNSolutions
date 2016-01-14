1800a128ae81d104be3d7241e7e664094a28aa46
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-15 00:29:27 +0530

    Uploading 2nd Ans

---
+++ A1-Q1A2.C
@@ -0,0 +1,14 @@
+# include<stdio.h>
+# include<conio.h>
+void main()
+{
+int sub,a,b;
+clrscr();
+printf("enter the no.");
+scanf("%d",&a);
+printf("enter 2nd no.");
+printf("%d",b);
+sub=a-b;
+printf("value is \t %d",sub);
+getch();
+}
