d397354af7c5acf81a0f57ce56bf845bed49d1d3
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-16 00:06:25 +0530

    no message

---
+++ 2/A2-Q2.C
@@ -0,0 +1,14 @@
+# include<stdio.h>
+# include<conio.h>
+void main()
+{
+char ch;
+clrscr();
+printf("specify the alphabet");
+scanf("%c",&ch);
+if(ch=='a'||'A'||'e'||'E'||'i'||'I'||'o'||'O'||'u'||'U')
+printf("It is a vowel");
+else
+printf("It is a not a vowel");
+getch();
+}
