44dd059e3054c669f36b1ec20d5ef2a93463d6de
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-17 01:18:37 +0530

    no message

---
+++ 2/A2-Q3.C
@@ -0,0 +1,19 @@
+# include<stdio.h>
+# include<conio.h>
+void main()
+{
+int y;
+clrscr();
+printf("Specify the Number");
+scanf("%d",&y);
+
+if(y%2==0)
+{
+printf("The no. is even");
+}
+else
+{
+printf("the no. is odd");
+}
+getch();
+}
