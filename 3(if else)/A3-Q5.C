aa9643d116a9c168de62a0d2b85279ec877ea92e
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-18 01:40:25 +0530

    no message

---
+++ 3(if else)/A3-Q5.C
@@ -0,0 +1,33 @@
+# include <stdio.h>
+# include <conio.h>
+void main()
+{
+int day;
+clrscr();
+printf("Specify the Days");
+scanf("%d",&day);
+
+if(day<=5)
+{
+printf("Fine is 50 paise");
+}
+else
+{
+	if(day<=10 && day>=6)
+	{
+	printf("Fine is 1 Rupee");
+	}
+	else
+	{
+		if(day>10 && day<=30)
+		{
+		printf("Fine is 5 Rupees");
+		}
+		else
+		{
+		printf("Membership Cancelled");
+		}
+	}
+}
+getch();
+}
