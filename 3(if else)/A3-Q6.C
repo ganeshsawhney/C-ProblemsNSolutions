31259be9aad8cc7d8985cd10c2839c920a491300
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-19 07:31:12 +0530

    no message

--- 3(if else)/A3-Q6.C
+++ 3(if else)/A3-Q6.C
@@ -1,3 +1,7 @@
+/* Created by Ganesh Sawhney       **
+** Email: ganeshsawhney@gmail.com  **
+** Github Username: ganeshsawhney  */
+
 # include <stdio.h>
 # include <conio.h>
 void main()
@@ -25,3 +29,7 @@ printf("Rate is 5 Rupees");
 }
 getch();
 }
+
+/* Created by Ganesh Sawhney       **
+** Email: ganeshsawhney@gmail.com  **
+** Github Username: ganeshsawhney  */
