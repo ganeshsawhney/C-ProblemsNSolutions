06ff0c846a60562a57be83d9ebdd877573ceaf1c
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-19 07:30:30 +0530

    no message

---
+++ 3(if else)/A3-Q7.C
@@ -0,0 +1,44 @@
+# include<stdio.h>
+# include<conio.h>
+void main()
+{
+int ttyl,lp,clyr;
+float yr;
+clrscr();
+printf("specify the year");
+scanf("%f",&yr);
+
+clyr=yr-1900;
+lp=clyr/4;
+ttyl=clyr+lp;
+
+if(ttyl%7==0)
+{
+printf("Day is Monday");
+}
+else if(ttyl%7==1)
+{
+printf("Day is tuesday");
+}
+else if(ttyl%7==2)
+{
+printf("Day is wednesday");
+}
+else if(ttyl%7==3)
+{
+printf("Day is thursday");
+}
+else if(ttyl%7==4)
+{
+printf("Day is friday");
+}
+else if(ttyl%7==5)
+{
+printf("Day is saturday");
+}
+else if(ttyl%7==6)
+{
+printf("Day is sunday");
+}
+getch();
+}
