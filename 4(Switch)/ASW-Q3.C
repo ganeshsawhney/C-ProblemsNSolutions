a5781aa406d5d805b19f1fc4b3647878eb21f903
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-20 16:41:34 +0530

    no message

---
+++ 4(Switch)/ASW-Q3.C
@@ -0,0 +1,27 @@
+# include<stdio.h>
+# include<conio.h>
+void main()
+{
+char ch;
+clrscr();
+printf("specify the alphabet");
+scanf("%c",&ch);
+switch(ch)
+{
+case 'a':
+case 'A':
+case 'e':
+case 'E':
+case 'i':
+case 'I':
+case 'o':
+case 'O':
+case 'u':
+case 'U':
+	printf("It is a vowel");
+	break;
+default:
+	printf("It is a not a vowel");
+}
+getch();
+}
