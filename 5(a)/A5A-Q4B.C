1cd021a2885d6f9d6551e77b14dff8a9440d6a34
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-22 12:21:10 +0530

    no message

---
+++ 5(a)/A5A-Q4B.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,k,j,z;
+clrscr();
+for(i=7;i>=0;i--)
+{
+
+for(k=5;k>=11-(i*2);k--)
+{
+printf("*");
+}
+
+printf("\n");
+
+for(z=7-i;z>=0;z--)
+{
+printf(" ");
+}}
+getch();
+}
