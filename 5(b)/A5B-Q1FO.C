8b39d0abee4508545fbe941c272fb1a3eb154c1b
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-24 02:46:47 +0530

    no message

--- 5(b)/A5B-Q1FO.C
+++ 5(b)/A5B-Q1FO.C
@@ -1,3 +1,7 @@
+/* Created by Ganesh Sawhney       **
+** Email: ganeshsawhney@gmail.com  **
+** Github Username: ganeshsawhney  */
+
 #include<stdio.h>
 #include<conio.h>
 void main()
@@ -22,3 +26,6 @@ else{;}
 }
 getch();
 }
+/* Created by Ganesh Sawhney       **
+** Email: ganeshsawhney@gmail.com  **
+** Github Username: ganeshsawhney  */
