a979ac7fc9ed17c4bcfec8a62a1da6b4dddc0df8
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-24 02:46:20 +0530

    no message

---
+++ 5(b)/A5B-Q2.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float z,sub,e=1,i,num,fact=1;
+clrscr();
+for(num=1;num>0;num++)
+{
+	for(i=num;i>=1;i--)
+	{
+	fact=fact*i;
+	}
+z=e+(1/fact);
+sub=z-e;
+if(sub<=0.00001)
+{
+printf("e= %f",z);
+break;
+}
+else
+{
+e=z;
+}
+}
+getch();
+}
