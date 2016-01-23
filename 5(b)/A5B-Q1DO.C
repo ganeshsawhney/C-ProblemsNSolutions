851000d05e4d8bb9e643aeb07652f0240d9d4154
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-24 02:45:05 +0530

    no message

---
+++ 5(b)/A5B-Q1DO.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float s,e,div,i=0,prod;
+clrscr();
+printf("Enter starting value");
+scanf("%f", &s);
+printf("Enter ending value");
+scanf("%f", &e);
+printf("Enter divisor");
+scanf("%f", &div);
+do
+{
+prod = div * i;
+i=i++;
+if(prod>=s && prod<=e)
+{
+printf("%f",prod);
+printf("\n");
+}
+else{;}
+}
+while(i<=e);
+getch();
+}
