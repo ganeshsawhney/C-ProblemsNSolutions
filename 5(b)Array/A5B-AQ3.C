44c4e138ef341ba9b290f9f6c562f4f196ea86e3
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-25 01:34:36 +0530

    no message

---
+++ 5(b)Array/A5B-AQ3.C
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,temp,a[100],b;
+clrscr();
+printf("Enter total numbers to be entered");
+scanf("%d", &b);
+for (i=0;i<b;i++)
+{
+printf("Enter the element");
+scanf("%d",&a[i]);
+}
+if(b%2==0)
+{
+	for(i=0;i<b/2;i++)
+	{
+	temp=a[i];
+	a[i]=a[b-1-i];
+	a[b-1-i]=temp;
+	}
+}
+else if (b%2==1)
+{
+	for(i=0;i<(b-1)/2;i++)
+	{
+	temp=a[i];
+	a[i]=a[b-1-i];
+	a[b-1-i]=temp;
+	}
+}
+for(i=0;i<b;i++)
+{
+printf("%d",a[i]);
+}
+getch();
+}
