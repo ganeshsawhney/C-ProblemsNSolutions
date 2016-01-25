f1317c410eefca7bfd13fe087734dcedc727ff98
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-26 00:09:35 +0530

    no message

---
+++ 6(a)/A6A-Q3.C
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int nm,i,j,l,a[100];
+clrscr();
+printf("Enter no. of elements to be entered");
+scanf("%d", &nm);
+for (i=0;i<nm;i++)
+{
+printf("Enter a[%d]",i);
+scanf("%d",&a[i]);
+}
+
+for (i=0;i<nm;i++)
+{
+for(j=i+1;j<nm;j++)
+{
+	if(a[i]==a[j])
+	{
+		for(l=j;l<nm;l++)
+		{
+		a[l]=a[l+1];
+		}
+		nm= nm-1;
+		j=j--;
+	}
+	else
+	{}
+}
+}
+for(i=0;i<nm;i++)
+{
+printf("\n %d ",a[i]);
+}
+getch();
+}
