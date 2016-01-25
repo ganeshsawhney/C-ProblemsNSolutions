b1a9b83667eb94cf2bec94718440f222c4833640
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-26 00:09:00 +0530

    no message

---
+++ 6(a)/A6A-Q1.C
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,sz,j,y,or,ck,a[100];
+clrscr();
+printf("Enter Size");
+scanf("%d", &sz);
+for (y=0;y<sz;y++)
+{
+printf("Enter a[%d]",y);
+scanf("%d",&a[y]);
+}
+for (i=0;i<sz;i++)
+{
+for (j=0;j<sz;j++)
+{
+if(i==j)
+{;
+}
+else
+{
+	if(a[i]>a[j])
+	{;
+	}
+	else
+	{
+	a[i]=0;
+	}
+}}
+}
+for(i=0;i<sz;i++)
+{
+if(a[i]>0)
+{
+printf("Largest is %d",a[i]);
+}
+else
+{}
+}
+getch();
+}
