3cfa4eb462da27c5b852d8f51fca40304e8a2816
ganeshsawhney <ganeshsawhney@gmail.com>
2016-01-27 02:48:18 +0530

    no message

---
+++ 6(b)/A6B-Q1.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,r,c,a[100][100];
+clrscr();
+printf("Enter no. of rows to be entered");
+scanf("%d", &r);
+printf("Enter no. of columns to be entered");
+scanf("%d", &c);
+for (i=0;i<(r*c);i++)
+{
+for (j=0;j<(r*c);j++)
+{
+printf("Enter a[%d][%d]",i,j);
+scanf("%d",&a[i][j]);
+}
+}
+
+for (i=0;i<(r*c);i++)
+{
+for (j=0;j<(r*c);j++)
+{
+printf("Element a[%d][%d] is %d\n",i,j,a[i][j]);
+}
+}
+getch();
+}
