371fe87993e661d999c254fbed35ca955aa2e71e
ganeshsawhney <ganeshsawhney@gmail.com>
2016-02-01 00:02:17 +0530

    no message

---
+++ 9(b)/A9B-Q1.C
@@ -0,0 +1,39 @@
+# include<stdio.h>
+# include<conio.h>
+int vwl();
+void main()
+{
+int val;
+clrscr();
+val=vwl();
+printf("\nValue returned is\t%d",val);
+getch();
+}
+int vwl()
+{
+char b[100] ,word;
+int i , ttl,value=0,temp1;
+for(i=0;i<100;i++)
+{
+printf("\nEnter %d character\t",i+1);
+scanf("%s",b[i]);
+printf("\n to Enter more press 1 else 2\t");
+scanf("%d",&temp1);
+if(temp1==2)
+{
+ttl=i;
+break;
+}
+}
+printf("\nEnter char to check\t");
+scanf("%s",word);
+for(i=0;i<=ttl;i++)
+{
+if(b[i]==word)
+{
+value=1;
+break;
+}
+}
+return(value);
+}
