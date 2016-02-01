4ffbc0b43ce5bf47ef7a2e85748c6150e8ba6f64
ganeshsawhney <ganeshsawhney@gmail.com>
2016-02-02 01:49:14 +0530

    no message

---
+++ 10(a)/A10A-Q1.C
@@ -0,0 +1,27 @@
+# include<stdio.h>
+# include<conio.h>
+int sum(int x);
+void main()
+{
+int num,sm;
+clrscr();
+printf("Enter the number\t\t");
+scanf("%d",&num);
+sm=sum(num);
+printf("\nSum is %d",sm);
+getch();
+}
+
+int sum(int x)
+{
+int sm;
+if(x<=0)
+{
+return (0);
+}
+else
+{
+sm=x+sum(x-1);
+}
+return (sm);
+}
