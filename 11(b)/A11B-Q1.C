f19313cd1809cbe5bb1d99b3805dc0f937357b14
ganeshsawhney <ganeshsawhney@gmail.com>
2016-02-03 00:04:26 +0530

    no message

---
+++ 11(b)/A11B-Q1.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+int *v1,*v2,*temp1;
+clrscr();
+printf("Enter Variable 1\t");
+scanf("%d",&*v1);
+printf("Enter Variable 2\t");
+scanf("%d",&*v2);
+*temp1=*v1;
+*v1=*v2;
+*v2=*temp1;
+printf("\nVariable 1 \t%d\nVariable 2 \t %d",*v1,*v2);
+getch();
+}
