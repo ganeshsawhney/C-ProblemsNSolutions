7f9f1bfa95a21146d83a40a2efe49ca08f7fbf2b
ganeshsawhney <ganeshsawhney@gmail.com>
2016-02-04 00:46:05 +0530

    no message

---
+++ 12 P/A12P-Q2.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int fun();
+void main()
+{
+printf("%d",fun());
+getch();
+}
+int fun()
+{
+char *str[12];
+int x=1,i,j,ln;
+clrscr();
+printf("Enter String to be checked\n");
+gets(*str);
+ln=strlen(*str);
+for(i=0;*str[i]!='\0';i++)
+{
+if(*str[i]==*str[ln-1-i])
+{}
+else
+{
+x=0;
+break;
+}
+}
+return (x);
+}
