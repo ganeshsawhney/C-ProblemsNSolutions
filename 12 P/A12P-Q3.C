f96a572b3f7e7f62065c56d17773ab3666d1e33e
ganeshsawhney <ganeshsawhney@gmail.com>
2016-02-05 07:54:53 +0530

    no message

---
+++ 12 P/A12P-Q3.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+void main()
+{
+char *str[12],*temp[12];
+int i,j,ln;
+clrscr();
+printf("Enter String to be reversed\n");
+scanf("%s",str);
+ln=strlen(str) ;
+for(i=0;*str[i]!='\0';i++)
+{
+*temp[i]=*str[ln-i-1];
+}
+*temp[i+1]='\0';
+printf("%s",temp);
+getch();
+}
